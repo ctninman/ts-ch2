5e511aa3a0550d39e98d46cda896d5885b9f70ac
ctninman <89664157+ctninman@users.noreply.github.com>
2022-10-14 15:09:16 -0400

    first commit

---
+++ src/demo.ts
@@ -0,0 +1,16 @@
+let x: number
+let y: string
+let z: boolean
+let a: Date
+let b: string[]
+let c: any
+
+b = ["hello", 5]
+
+//not recommended
+b = 'happy' as any
+b = 4
+
+c = ["hello", 5]
+c = 'happy'
+c = 4
