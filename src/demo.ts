e1a8781ce82afe649a1006a245bdb8cd83206b78
ctninman <89664157+ctninman@users.noreply.github.com>
2022-10-14 15:52:53 -0400

    programmed interface

--- src/demo.ts
+++ src/demo.ts
@@ -5,12 +5,32 @@ let a: Date
 let b: string[]
 let c: any
 
-b = ["hello", 5]
+// b = ["hello", 5]
 
 //not recommended
-b = 'happy' as any
-b = 4
+// b = 'happy' as any
+// b = 4
 
-c = ["hello", 5]
-c = 'happy'
-c = 4
+// c = ["hello", 5]
+// c = 'happy'
+// c = 4
+
+interface Contact extends Address{
+	id: number;
+	name: string;
+	birthDate?: Date;
+}
+
+interface Address {
+	line1: string;
+	line2?: string;
+	postalCode: string;
+}
+
+let primaryContact: Contact = {
+	// birthDate: new Date("01-01-1999"),
+	id: 123,
+	name: "bigPoppa",
+	postalCode: '11226',
+	line1: '385 E ...'
+}
